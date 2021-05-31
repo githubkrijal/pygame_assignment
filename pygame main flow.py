d9f546f72213b6dc2c0fa3b3eb3501d1c2212a65
Admin <krijalshrestha32@gmail.com>
2021-05-31 18:29:11 +0545

    pygame

--- pygame main flow.py
+++ pygame main flow.py
@@ -18,6 +18,9 @@ bg = pygame.image.load("BG3.jpg")
 idle_right = pygame.image.load("idelr.png")
 idle_left = pygame.image.load("idlel.png")
 character = pygame.image.load("robot.png")
+
+
+
 #bulletround = pygame.image.load("bullet1.png")
 
 clock = pygame.time.Clock()
@@ -75,6 +78,49 @@ class projectile(object):
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
 
+class enemy(object):
+    walkright = [pygame.image.load("planeright1.png"), pygame.image.load("planeright2.png")]
+    walkleft = [pygame.image.load("planeleft1.png"), pygame.image.load("planeleft2.png")]
+
+    def __init__(self,x, y, width , height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walkCount = 0
+        self.vel = 3
+
+    def draw(self,win):
+        self.move()
+        if self.walkCount + 1 >= 6:
+            self.walkCount = 0
+
+        if self.vel > 0:
+            win.blit(self.walkleft[self.walkCount//3], (self.x, self.y))
+            self.walkCount +=1
+        else:
+            win.blit(self.walkright[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        pass
+
+    def move(self):
+        if  self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x -= self.vel
+            else:
+                self.vel = self.vel * +1
+                self.walkCount = 0
+        else:
+            if self.x + self.vel > self.path[0]:
+                self.x -= self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+
+
+
 
 
 def redrawGameWindow():
@@ -82,6 +128,8 @@ def redrawGameWindow():
     win.blit(bg, (0,0))
     #calling robot from class
     robot.draw(win)
+
+    plane.draw(win)
 
     for bullet in bullets:
         bullet.draw(win)
@@ -93,6 +141,7 @@ def redrawGameWindow():
 
 #creating a main loop
 robot = player(300, 370, 80, 80)
+plane = enemy(800,100, 100,100, 1000)
 bullets = []
 run = True
 while run:
